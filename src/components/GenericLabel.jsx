2881983436f37b0ddf36198b6e92aa4cb201633d
Ridwan Abiola <ricklick262@gmail.com>
2023-05-29 12:48:54 +0100

    fix: unecessary code

--- src/components/GenericLabel.jsx
+++ src/components/GenericLabel.jsx
@@ -1,15 +1,13 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function GenericLabel({heading, text}) {
+export default function GenericLabel({ heading, text }) {
   return (
     <Box>
       <Heading as="h2" fontSize="1.125em" color="#05365c" marginBottom="1.5">
-        {/* Card Number */}
         {heading}
       </Heading>
       <Text color="#8e96a3" fontWeight="medium">
-        {/* Enter the 15-digit number on the card */}
         {text}
       </Text>
     </Box>
@@ -17,8 +15,6 @@ export default function GenericLabel({heading, text}) {
 }
 
 GenericLabel.propTypes = {
-    heading: PropTypes.string,
-    text: PropTypes.string,
-}
-
-
+  heading: PropTypes.string,
+  text: PropTypes.string,
+};
